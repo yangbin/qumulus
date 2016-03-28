a9aaf2614b81d3f7b209fd4ccc5cfeae9233ebd8
Kwok Yang Bin <yangbin@fragnetics.com>
2016-03-28 16:50:29 +0800

    Convert Zone to a messaging-passing thread

--- src/manager.rs
+++ src/manager.rs
@@ -1,7 +1,7 @@
 //! Zone registry, dispatches commands and spawns Zones
 
 use std::collections::BTreeMap;
-use std::sync::{Arc, RwLock};
+use std::sync::{Arc, Mutex, RwLock};
 use std::sync::mpsc;
 
 use serde_json::Value;
@@ -9,9 +9,10 @@ use serde_json::Value;
 use command::Command;
 use path::Path;
 use zone::Zone;
+use zone::ZoneHandle;
 
 pub struct Manager {
-    active: RwLock<BTreeMap<Path, Arc<Zone>>>
+    active: RwLock<BTreeMap<Path, Arc<Mutex<ZoneHandle>>>>
 }
 
 impl Manager {
@@ -19,16 +20,16 @@ impl Manager {
         Arc::new(Manager { active: RwLock::new(BTreeMap::new()) })
     }
 
-    pub fn load(&self, path: Path) {
+    pub fn load(&self, path: &Path) {
         if self.zone_loaded(&path) {
             return;
         }
 
         let mut active = self.active.write().unwrap();
 
-        let zone = Arc::new(Zone::new(path.clone()));
+        let zone = Arc::new(Mutex::new(Zone::spawn(path)));
 
-        active.insert(path, zone);
+        active.insert(path.clone(), zone);
     }
 
     pub fn zone_loaded(&self, path: &Path) -> bool {
@@ -43,16 +44,16 @@ impl Manager {
         zone.dispatch(command, tx)
     }
 
-    pub fn find_nearest(&self, path: &Path) -> Arc<Zone> {
+    pub fn find_nearest(&self, path: &Path) -> ZoneHandle {
         let active = self.active.read().unwrap();
 
         // TODO actually find the nearest zone
         let zone = active.get(&Path::new(vec![]));
 
         match zone {
-            Some(zone) => zone.clone(),
+            Some(zone) => zone.lock().unwrap().clone(),
             None => {
-                unimplemented!();
+                unimplemented!()
             }
         }
     }
--- src/zone.rs
+++ src/zone.rs
@@ -1,8 +1,12 @@
 //! Owns a subtree of entire tree, also unit of concurrency
+//!
+//! The Zone structure represents the subtree and is run as a thread.
+//! ZoneHandle is the public interface to a single zone.
 
 use std::collections::BTreeMap;
-use std::sync::{Arc, RwLock};
+use std::sync::mpsc;
 use std::sync::mpsc::Sender;
+use std::thread;
 
 use serde_json::Value;
 
@@ -14,39 +18,81 @@ use node::Vis;
 use node::Update;
 use path::Path;
 
-// TODO: Consider Zone as a thread
-
 #[derive(Debug)]
 pub struct ZoneData {
     node: Node, // Mergeable data for this Zone
     vis: Vis    // Visibility of this Zone through ancestors
 }
 
+#[derive(Clone)]
+pub struct ZoneHandle {
+    tx: Sender<ZoneCommand>
+}
+
+struct ZoneCommand {
+    command: Command,
+    reply: Sender<Value>,
+    listener: Sender<String>
+}
+
 pub struct Zone {
-    path: Path,                      // Path to this Zone
-    data: RwLock<ZoneData>,          // 'Atomic' data for this Zone
-    listeners: RwLock<Vec<Listener>> // List of binds
+    path: Path,               // Path to this Zone
+    data: ZoneData,           // 'Atomic' data for this Zone
+    listeners: Vec<Listener>, // List of binds
+    writes: u64               // Number of writes since last fragment check
     // TODO: size: u64,
     // TODO: prefixes: Option<BTreeMap<String, Node>>
     // TODO: replicas: Vec<Replicas>
 }
 
+impl ZoneHandle {
+    pub fn dispatch(&self, command: Command, listener: &Sender<String>) -> Value {
+        let (tx, rx) = mpsc::channel();
+
+        let command = ZoneCommand { command: command, reply: tx, listener: listener.clone() };
+
+        self.tx.send(command).unwrap();
+        rx.recv().unwrap()
+    }
+}
+
 impl Zone {
-    pub fn new(path: Path) -> Zone {
+    pub fn spawn(path: &Path) -> ZoneHandle {
+        let (tx, rx) = mpsc::channel();
+
+        let zone = Zone::new(path);
+
+        thread::spawn(move|| {
+            zone.message_loop(rx);
+        });
+
+        ZoneHandle { tx: tx }
+    }
+
+    pub fn new(path: &Path) -> Zone {
         Zone {
             path: path.clone(),
-            data: RwLock::new(ZoneData {
+            data: ZoneData {
                 node: Node::expand(&Value::Null, 0),
                 vis: match path.path.len() {
                     0 => Vis::new(1, 0),
                     _ => Default::default()
                 }
-            }),
-            listeners: RwLock::new(vec![])
+            },
+            listeners: vec![],
+            writes: 0
         }
     }
 
-    pub fn dispatch(&self, command: Command, tx: &Sender<String>) -> Value {
+    fn message_loop(mut self, rx: mpsc::Receiver<ZoneCommand>) {
+        for cmd in rx {
+            let result = self.dispatch(cmd.command, &cmd.listener);
+
+            cmd.reply.send(result).unwrap(); // TODO: don't crash the Zone!
+        }
+    }
+
+    pub fn dispatch(&mut self, command: Command, tx: &Sender<String>) -> Value {
         match command.call {
             Call::Bind => {
                 self.bind(&command.path, tx)
@@ -66,7 +112,7 @@ impl Zone {
     }
 
     /// Bind value(s)
-    pub fn bind(&self, path: &Path, tx: &Sender<String>) -> Value {
+    pub fn bind(&mut self, path: &Path, tx: &Sender<String>) -> Value {
         // TODO verify path
 
         self.sub(path, tx);
@@ -74,22 +120,32 @@ impl Zone {
     }
 
     /// Kill value(s)
-    pub fn kill(&self, path: &Path, ts: u64) {
+    pub fn kill(&mut self, path: &Path, ts: u64) {
         let node = Node::delete(ts);
 
-        let mut diff = node.prepend_path(&path.path);
+        let diff = node.prepend_path(&path.path);
 
-        let mut data = self.data.write().unwrap();
+        self.merge(diff);
+        // TODO: externals goes to external nodes
+        // TODO: diff goes to replicas
+    }
 
+    pub fn merge(&mut self, mut diff: Node) {
         let (update, _) = {
-            let ZoneData { ref mut node, ref mut vis } = *data;
+            let ZoneData { ref mut node, vis } = self.data;
 
-            node.merge(&mut diff, *vis, *vis)
+            node.merge(&mut diff, vis, vis)
         };
 
         if let Some(update) = update {
             self.notify(&update);
+            self.writes += 1;
+
+            if self.writes >= 1000 {
+                self.split_check();
+            }
         }
+
         // TODO: externals goes to external nodes
         // TODO: diff goes to replicas
     }
@@ -98,9 +154,7 @@ impl Zone {
     pub fn read(&self, path: &Path) -> Value {
         // TODO verify path
 
-        let data = self.data.read().unwrap();
-
-        let read = data.node.read(data.vis, path);
+        let read = self.data.node.read(self.data.vis, path);
 
         let (update, _) = read;
 
@@ -110,38 +164,31 @@ impl Zone {
     }
 
     /// Writes value(s) to the node at `path` at time `ts`
-    pub fn write(&self, path: &Path, ts: u64, value: Value) {
+    pub fn write(&mut self, path: &Path, ts: u64, value: Value) {
         // TODO verify path
-        let mut diff = Node::expand_from(&path.path[..], &value, ts);
+        let diff = Node::expand_from(&path.path[..], &value, ts);
 
-        let mut data = self.data.write().unwrap();
-
-        let (update, _) = {
-            let ZoneData { ref mut node, ref mut vis } = *data;
-
-            node.merge(&mut diff, *vis, *vis)
-        };
-
-        if let Some(update) = update {
-            self.notify(&update);
-        }
-        // TODO: externals goes to external nodes
-        // TODO: diff goes to replicas
+        self.merge(diff);
     }
 
     fn notify(&self, update: &Update) {
-        let listeners = self.listeners.read().unwrap();
-
-        for listener in &*listeners {
-            listener.update(update);
+        for listener in &self.listeners {
+            listener.update(update).unwrap();
+            // TODO: don't crash Zone; remove listener from list
         }
     }
 
-    fn sub(&self, path: &Path, tx: &Sender<String>) {
-        let mut listeners = self.listeners.write().unwrap();
-
+    fn sub(&mut self, path: &Path, tx: &Sender<String>) {
         let listener = Listener::new(path, tx);
 
-        listeners.push(listener);
+        self.listeners.push(listener);
+    }
+
+    fn split_check(&mut self) {
+        println!("Checking if split needed, {} writes", self.writes);
+
+        // TODO
+
+        self.writes = 0;
     }
 }
